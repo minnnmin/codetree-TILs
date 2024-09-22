a814b30f7acc0c9f2b0af6dff5a247e1ce3bda16
minnnmin <38430523+minnnmin@users.noreply.github.com>
2024-09-22 18:23:40 +0900

    Title: 외주 수익 최대화하기, Time: 188ms, Memory: 26MB, Status: Passed - CodeTree

--- 240922/외주 수익 최대화하기/max-of-outsourcing-profit.py
+++ 240922/외주 수익 최대화하기/max-of-outsourcing-profit.py
@@ -1,25 +1,31 @@
 N = int(input())
-jobs = [] # [id, 기간, 수익]
+jobs = [] # [시작 가능 일, 기간, 수익]
 for i in range(N):
-    jobs.append([i] + list(map(int, input().split())))
+    jobs.append([i+1] + list(map(int, input().split())))
 
 max_profit = 0
-visited = [False] * N
+visited = [False] * (N+1)
 
-def func(n, sum_profit):
+# job_start_day: 새 작업 시작 가능일
+# job_end_day: 이전 작업 종료일
+def func(job_start_day, job_end_day, sum_profit):
     global max_profit
 
-    if n > N:
+    if job_end_day > N:
         return
     else:
         max_profit = sum_profit if sum_profit > max_profit else max_profit
 
     for i, days, profit in jobs:
-        if not visited[i]:
+        if not visited[i] and i == job_start_day:
             visited[i] = True
-            func(n + days, sum_profit + profit)
+            func(job_start_day + days, job_end_day + days, sum_profit + profit)
+            visited[i] = False
+        elif not visited[i] and i > job_start_day:
+            visited[i] = True
+            func(i + days, i + days - 1, sum_profit + profit)
             visited[i] = False
 
-func(0, 0)
+func(1, 0, 0)
 
 print(max_profit)
