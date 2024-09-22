715d52ac7be972c14b3e15ef403ddb10b76a4541
minnnmin <38430523+minnnmin@users.noreply.github.com>
2024-09-22 23:13:50 +0900

    Title: 연산자 배치하기, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240922/연산자 배치하기/arrange-operator.py
@@ -0,0 +1,41 @@
+N = int(input())
+numbers = list(map(int, input().split()))
+plus, minus, mul = map(int, input().split())
+operators = ['+']*plus + ['-']*minus + ['*']*mul
+
+
+# opers는 최종 선택된 연산자들
+def evaluate(opers):
+    answer = numbers[0]
+    i = 1
+    for op in opers:
+        if op == '+':
+            answer += numbers[i]
+        elif op == '-':
+            answer -= numbers[i]
+        elif op == '*':
+            answer *= numbers[i]
+        i += 1
+    return answer
+
+
+max_val = -1e9
+min_val = 1e9
+visited = [False] * len(operators)
+
+def func(n, result):
+    global max_val, min_val
+    if n == N-1:
+        answer = evaluate(result)
+        max_val = answer if answer > max_val else max_val
+        min_val = answer if answer < min_val else min_val
+        return
+
+    for i in range(len(operators)):
+        if not visited[i]:
+            visited[i] = True
+            func(n+1, result + [operators[i]])
+            visited[i] = False
+
+func(0, [])
+print(min_val, max_val)
