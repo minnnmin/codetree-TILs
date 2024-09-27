5e6356ae9e66f99743abe733aac394596714366a
minnnmin <38430523+minnnmin@users.noreply.github.com>
2024-09-27 20:10:21 +0900

    Title: 토스트 계란틀, Time: 151ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240927/토스트 계란틀/toast-eggmold.py
+++ 240927/토스트 계란틀/toast-eggmold.py
@@ -12,53 +12,48 @@ def in_range(x, y):
 
 # 그룹으로 묶일 계란들 탐색
 # start = (x, y)
-def bfs(start):
 
-    q = deque([start])
-    visited = [[False for _ in range(N)] for _ in range(N)]
-    visited[start[0]][start[1]] = True
-    group = [start]
-    eggs_sum = EGGS[start[0]][start[1]]
+
+q = deque()
+visited = [[False for _ in range(N)] for _ in range(N)]
+
+def bfs():
 
     while q:
         x, y = q.popleft()
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+            nx, ny = x + dx[i], y + dy[i]
             if in_range(nx, ny) and not visited[nx][ny] and L <= abs(EGGS[x][y] - EGGS[nx][ny]) <= R:
                 q.append((nx, ny))
                 visited[nx][ny] = True
                 group.append((nx, ny))
-                eggs_sum += EGGS[nx][ny]
-
-    l = len(group)
-    for r, c in group:
-        TMP_EGGS[r][c] = eggs_sum // l
-
-    return True if l > 1 else False
 
 
-# VISITED = [[0 for _ in range(N)] for _ in range(N)]
 for sec in range(1999):
-    available = False
+    is_changed = False
     # 합쳐질 계란들 집합 구하기
     for i in range(N):
         for j in range(N):
             if not TMP_EGGS[i][j]:
-                res = bfs((i, j))
-                if res == True:
-                    available = True
+                group = []
+                
+                q.append((i, j))
+                group.append((i, j))
+                visited[i][j] = True
+                bfs()
 
-    if not available:
-        break       
+                if len(group) > 1:
+                    is_changed = True
 
-    for i in range(N):
-        for j in range(N):
-            EGGS[i][j] = TMP_EGGS[i][j]
-            TMP_EGGS[i][j] = 0
+                sum_of_eggs = 0
+                for x, y in group:
+                    sum_of_eggs += EGGS[x][y]
+                
+                for x, y in group:
+                    EGGS[x][y] == sum_of_eggs // len(group)
 
-    # for _ in EGGS:
-    #     print(_)
-    # print()
+
+    if not is_changed:
+        break
 
 print(sec)
