64ec5ee71bef6b8b93c72ea11a81019e8f16c81f
minnnmin <38430523+minnnmin@users.noreply.github.com>
2024-09-27 19:38:47 +0900

    Title: 토스트 계란틀, Time: 136ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240927/토스트 계란틀/toast-eggmold.py
+++ 240927/토스트 계란틀/toast-eggmold.py
@@ -2,27 +2,17 @@ from collections import deque
 
 N, L, R = map(int, input().split())
 EGGS = [list(map(int, input().split())) for _ in range(N)]
-
-
-# 0이 아니라면 BFS가 지나간 곳임. 이 역시 매 초마다 초기화
-# VISITED = [[0 for _ in range(N)] for _ in range(N)]
-
+TMP_EGGS = [[0 for _ in range(N)] for _ in range(N)]
 
 # 상하좌우
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-
-
 def in_range(x, y):
     return -1 < x < N and -1 < y < N
 
-
 # 그룹으로 묶일 계란들 탐색
-# GROUP에 gid 표시
 # start = [x, y]
-def bfs(start, gid):
-
-    VISITED[start[0]][start[1]] = gid
+def bfs(start):
 
     q = deque([start])
     visited = [[False for _ in range(N)] for _ in range(N)]
@@ -37,42 +27,37 @@ def bfs(start, gid):
             ny = y + dy[i]
             if in_range(nx, ny) and not visited[nx][ny] and L <= abs(EGGS[x][y] - EGGS[nx][ny]) <= R:
                 q.append([nx, ny])
-                VISITED[nx][ny] = gid
                 visited[nx][ny] = True
                 group.append([nx, ny])
                 eggs_sum += EGGS[nx][ny]
 
-    return group, eggs_sum
+    l = len(group)
+    for r, c in group:
+        TMP_EGGS[r][c] = eggs_sum // l
 
-VISITED = [[0 for _ in range(N)] for _ in range(N)]
-group_and_eggsum = [] # [group에 속하는 애들 좌표리스트, 계란의 합]
-for sec in range(2000):
-    group_and_eggsum.clear()
-    # for x in range(N):
-    #     for y in range(N):
-    #         VISITED[x][y] = 0
+    return True if l > 1 else False
+
+
+# VISITED = [[0 for _ in range(N)] for _ in range(N)]
+for sec in range(2):
     available = False
     # 합쳐질 계란들 집합 구하기
     for i in range(N):
         for j in range(N):
-            if not VISITED[i][j]:
-                group, eggs_sum = bfs([i, j], (sec, i, j))
-                if len(group) != 1:
-                    group_and_eggsum.append([group, eggs_sum])
+            if not TMP_EGGS[i][j]:
+                res = bfs([i, j])
+                if res == True:
                     available = True
-    # print(group, eggs_sum)
-    if not available:
-        break         
 
-    # 계란 합치기 진행
-    for g, es in group_and_eggsum:
-        l = len(g)
-        for x, y in g:
-            EGGS[x][y] = es // l
+    if not available:
+        break       
+
+    for i in range(N):
+        for j in range(N):
+            EGGS[i][j] = TMP_EGGS[i][j]
+            TMP_EGGS[i][j] = 0
 
     # for _ in EGGS:
     #     print(_)
-    # print()
-
 
 print(sec)
