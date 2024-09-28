be682eaf558b4e0c7552aa1d183a84a4e397d146
minnnmin <38430523+minnnmin@users.noreply.github.com>
2024-09-28 19:53:12 +0900

    Title: 청소는 즐거워, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240928/청소는 즐거워/cleaning-is-joyful.py
@@ -0,0 +1,15 @@
+N = int(input())
+dust = [list(map(int, input().split())) for _ in range(N)]
+
+tmp = []
+for i in range(1, N):
+    tmp.append(i)
+    tmp.append(i)
+tmp.append(N-1)
+
+# (이동방향, 이동크기)
+order_list = [] # [(0, 1), (1, 1), (2, 2), (3, 2), (0, 3), (1, 3), (2, 4), (3, 4), (0, 4)]
+for i in range(len(tmp)):
+    order_list.append((i%4, tmp[i]))
+
+ㄹ
