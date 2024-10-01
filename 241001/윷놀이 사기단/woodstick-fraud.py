36e5c10c2edeabcf70efdb44879427a833365e4c
minnnmin <38430523+minnnmin@users.noreply.github.com>
2024-10-01 12:55:07 +0900

    Title: 윷놀이 사기단, Time: 2912ms, Memory: 32MB, Status: Passed - CodeTree

--- 241001/윷놀이 사기단/woodstick-fraud.py
+++ 241001/윷놀이 사기단/woodstick-fraud.py
@@ -8,7 +8,8 @@ def get_next_pos(now, group, cnt):
     next_pos = -1
     next_group = group
 
-    # 시작 위치가 교차로인 경우 먼저 처리 # 교차로일 경우 그룹 '' 이거여야 함. 혹은 40을 초과할 때도.
+    # 시작 위치가 교차로인 경우 먼저 처리 
+    # 교차로일 경우 그룹 '' 이거여야 함. 혹은 40을 초과할 때도.
     if now == 10:
         if cnt <= 3:
             next_pos = 10 + cnt*3
@@ -19,7 +20,6 @@ def get_next_pos(now, group, cnt):
         elif cnt == 5:
             next_pos = 30
             next_group = 'H'
-
     elif now == 20:
         if cnt <= 2:
            next_pos = 20 + cnt*2
@@ -30,7 +30,6 @@ def get_next_pos(now, group, cnt):
         elif cnt >= 4:
             next_pos = 25 + (cnt-3)*5
             next_group = 'H'
-
     elif now == 30 and group == '':
         if cnt == 1:
             next_pos = 28
@@ -44,18 +43,15 @@ def get_next_pos(now, group, cnt):
         elif cnt == 5:
             next_pos = 30
             next_group = 'H'
-
     elif now == 25:
         next_pos = 25 + cnt*5
         if next_pos >= 40:
             next_group = ''
         else:
-            next_group = 'H'
-        
+            next_group = 'H'      
     elif now == 40:
         next_pos = 40 + cnt*1
         next_group = ''
-
     else:
         if group == 'A':
             next_pos = now + cnt*2
@@ -165,18 +161,12 @@ def get_next_pos(now, group, cnt):
             if next_pos >= 40:
                 next_group = ''
 
-
     return next_pos, next_group
 
 
 # players_pos = [[위치, 그룹], [], ..]
-def dfs(t, players_pos, history):
+def func(t, players_pos, history):
     global MAX_SCORE
-    # if len(history) == 6:
-    #     print(players_pos)
-    #     print('turn', t)
-    #     print(history)
-    #     print()
 
     done = True
     for pos, group in players_pos:
@@ -188,21 +178,14 @@ def dfs(t, players_pos, history):
             if his[3] <= 40:
                 score += his[3]
         MAX_SCORE = score if score > MAX_SCORE else MAX_SCORE
-        # print(history)
-        # print(players_pos)
-        # print()
         return
 
     if t == 10:
-        # score 계산
         score = 0
         for his in history:
             if his[3] <= 40:
                 score += his[3]
         MAX_SCORE = score if score > MAX_SCORE else MAX_SCORE
-        # print(history)
-        # print(players_pos)
-        # print()
         return
 
     # 진행
@@ -214,10 +197,10 @@ def dfs(t, players_pos, history):
         if next_pos > 40 or (next_pos, next_group) not in players_pos:
             players_pos[i] = (next_pos, next_group)
             history.append((t, pos, group, next_pos, next_group))
-            dfs(t+1, players_pos, history)
+            func(t+1, players_pos, history)
             players_pos[i] = (pos, group)
             history.pop()
 
     
-dfs(0, [(0, 'A'), (0, 'A'), (0, 'A'), (0, 'A')], [])
+func(0, [(0, 'A'), (0, 'A'), (0, 'A'), (0, 'A')], [])
 print(MAX_SCORE)
