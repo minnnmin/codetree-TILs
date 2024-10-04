8d3dadb63beaaeb2a4376c92fcb84678e92bb940
minnnmin <38430523+minnnmin@users.noreply.github.com>
2024-10-04 22:21:56 +0900

    Title: 병원 거리 최소화하기, Time: 5070ms, Memory: 29MB, Status: Time Limit Exceed - CodeTree

--- 241004/병원 거리 최소화하기/min-of-hospital-distance.py
+++ 241004/병원 거리 최소화하기/min-of-hospital-distance.py
@@ -28,18 +28,14 @@ def bfs(x, y):
     visited = [[False for _ in range(N)] for _ in range(N)]
     visited[x][y] = True
     q.append((x, y, 0))
-    # print('bfs 안에서1')
 
     while q:
         r, c, dis = q.popleft()
-        # print('r, c:', r, c)
         dis += 1
         for i in range(4):
             nr, nc = r+dx[i], c+dy[i]
-            # print('nr, nc:', nr, nc)
             if in_range(nr, nc) and not visited[nr][nc]:
                 if MATRIX[nr][nc] == 2: # 병원 발견
-                    # print('병원!')
                     return dis
                 else:
                     visited[nr][nc] = True
@@ -50,31 +46,24 @@ def bfs(x, y):
 
 # 현재 MATRIX 기준으로 병원거리 합 리턴
 def get_hospital_dist():
+    global ANSWER
     res = 0
-    # print('이걸 살펴볼 거다')
-    # for _ in MATRIX:
-    #     print(_)
     for x in range(N):
         for y in range(N):
             if MATRIX[x][y] == 1: # 사람이면 BFS 돌려서 병원거리 구하기
                 tmp = bfs(x, y)
                 res += tmp
                 q.clear()
-                # print('bfs 안에서', x, y, '에서의 병원거리는', tmp)
-    return res
+    ANSWER = res if res < ANSWER else ANSWER
 
 
 # history에는 이미 선택된 병원의 인덱스가 들어있음. 이것들보다 큰 것만 추가 가능
-def backtracking(n, history):
+def backtracking2(n, history):
     global ANSWER 
 
     if n == M:
         res = get_hospital_dist()
         ANSWER = res if res < ANSWER else ANSWER
-        # print('갱신')
-        # print(ANSWER, res)
-        # for _ in MATRIX:
-        #     print(_)
         return
 
     for new_pos_id in range(L):
@@ -86,5 +75,32 @@ def backtracking(n, history):
             MATRIX[nx][ny] = 0
             history.pop()
 
-backtracking(0, [])
+# m개씩 짝지어진 병원들 정보. m이 2일 때: [(병원1 위치, 병원2 위치), ... ]
+SELECTED_HOSPITALS = []
+def backtracking(n, history):
+    global SELECTED_HOSPITALS 
+
+    if n == M:
+        tmp = []
+        for i in history:
+            tmp.append(HOSPITALS[i])
+        SELECTED_HOSPITALS.append(tmp)
+        return
+
+    for new_pos_id in range(L):
+        if not history or new_pos_id > history[-1]:
+            history.append(new_pos_id)
+            nx, ny = HOSPITALS[new_pos_id]
+            backtracking(n+1, history)
+            history.pop()
+
+backtracking(0, []) # -> 남겨둘 병원 쌍이 저장되어 있음
+
+for pairs in SELECTED_HOSPITALS:
+    for x, y in pairs:
+        MATRIX[x][y] = 2
+    get_hospital_dist()
+    for x, y in pairs:
+        MATRIX[x][y] = 0
+
 print(ANSWER)
