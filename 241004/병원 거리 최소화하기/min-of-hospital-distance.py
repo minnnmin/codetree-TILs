f23d6d7b7dc69131ce027f698008c07f1c5b13ca
minnnmin <38430523+minnnmin@users.noreply.github.com>
2024-10-04 23:10:56 +0900

    Title: 병원 거리 최소화하기, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241004/병원 거리 최소화하기/min-of-hospital-distance.py
+++ 241004/병원 거리 최소화하기/min-of-hospital-distance.py
@@ -3,6 +3,9 @@ MATRIX = [list(map(int, input().split())) for _ in range(N)]
 HOSPITALS = []
 PEOPLE = []
 
+L = len(HOSPITALS)
+ANSWER = 1e9
+
 for x in range(N):
     for y in range(N):
         if MATRIX[x][y] == 2:
@@ -11,72 +14,9 @@ for x in range(N):
         elif MATRIX[x][y] == 1:
             PEOPLE.append((x, y))
 
-L = len(HOSPITALS)
-ANSWER = 1e9
-
-from collections import deque
-
-q = deque()
-
 # 상하좌우
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-
-def in_range(x, y):
-    return -1 < x < N and -1 < y < N
-
-def bfs(x, y):
-    # (x, y)에 있는 사람의 최소 병원거리
-    visited = [[False for _ in range(N)] for _ in range(N)]
-    visited[x][y] = True
-    q.append((x, y, 0))
-
-    while q:
-        r, c, dis = q.popleft()
-        dis += 1
-        for i in range(4):
-            nr, nc = r+dx[i], c+dy[i]
-            if in_range(nr, nc) and not visited[nr][nc]:
-                if MATRIX[nr][nc] == 2: # 병원 발견
-                    q.clear()
-                    return dis
-                else:
-                    visited[nr][nc] = True
-                    q.append((nr, nc, dis))
-
-    return 'err' # 이게 출력되면 안됨
-
-
-# 현재 MATRIX 기준으로 병원거리 합 리턴
-def get_hospital_dist():
-    global ANSWER
-    res = 0
-    for x, y in PEOPLE:
-        q.clear()
-        tmp = bfs(x, y)
-        res += tmp
-                
-    ANSWER = res if res < ANSWER else ANSWER
-
-
-# history에는 이미 선택된 병원의 인덱스가 들어있음. 이것들보다 큰 것만 추가 가능
-def backtracking2(n, history):
-    global ANSWER 
-
-    if n == M:
-        res = get_hospital_dist()
-        ANSWER = res if res < ANSWER else ANSWER
-        return
-
-    for new_pos_id in range(L):
-        if not history or new_pos_id > history[-1]:
-            history.append(new_pos_id)
-            nx, ny = HOSPITALS[new_pos_id]
-            MATRIX[nx][ny] = 2
-            backtracking(n+1, history)
-            MATRIX[nx][ny] = 0
-            history.pop()
-
 
 # m개씩 짝지어진 병원들 정보. m이 2일 때: [(병원1 위치, 병원2 위치), ... ]
 SELECTED_HOSPITALS = []
@@ -101,14 +41,13 @@ def backtracking(n, history):
 backtracking(0, []) # -> 남겨둘 병원 쌍이 저장되어 있음
 
 for pairs in SELECTED_HOSPITALS:
-    # pairs에 병원 m개 담겨있음
     res = 0
     for x, y in PEOPLE:
-        tmp = 1e9 # 이번 사람의 최소 병원거리
+        min_dis = 1e9
         for hx, hy in pairs:
-            tmp2 = abs(hx-x) + abs(hy-y)
-            tmp = tmp2 if tmp2 < tmp else tmp
-        res += tmp
+            tmp = abs(hx-x) + abs(hy-y)
+            min_dis = tmp if tmp < min_dis else min_dis
+        res += min_dis
     ANSWER = res if res < ANSWER else ANSWER
     for x, y in pairs:
         MATRIX[x][y] = 0
