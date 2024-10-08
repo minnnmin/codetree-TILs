6dd7527750f2297c94edeac293d83ece402eeeaa
minnnmin <38430523+minnnmin@users.noreply.github.com>
2024-10-08 18:51:28 +0900

    Title: 자율주행 전기차, Time: 781ms, Memory: 29MB, Status: Passed - CodeTree

--- 241008/자율주행 전기차/autonomous-electric-car.py
+++ 241008/자율주행 전기차/autonomous-electric-car.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 N, M, BATTERY = map(int, input().split())
 MATRIX = [list(map(int, input().split())) for _ in range(N)]
 
@@ -21,18 +23,12 @@ for i in range(M):
     PASSENGER_DEST.append((x2-1, y2-1))
     MATRIX[x-1][y-1] = i+1 # 승객번호 저장
 
-# print(MATRIX)
-# print(PASSENGER_POS)
-# print(PASSENGER_DEST)
-
 # 상우하좌
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
 def in_range(x, y):
     return -1 < x < N and -1 < y < N
-
-from collections import deque
 
 q = deque()
 
@@ -70,9 +66,9 @@ def get_min_passenger():
     return min_passenger, min_passenger_x, min_passenger_y, answer
 
 
-# 승객을 목적지로 데려다 주기 - bfs 돌려서 최단거리 구해내기
 q2 = deque()
 
+# 승객을 목적지로 데려다 주기 - bfs 돌려서 최단거리 구해내기
 def ride(pid):
     global MATRIX
 
@@ -105,28 +101,21 @@ def ride(pid):
 while True:
     # 1. 승객 고르기
     pid, x, y, dis = get_min_passenger()
-    # print('선택된 승객, 거리', pid, dis)
     if pid == -1: # 이동 가능한 승객 없음
         break
     BATTERY -= dis
-    # print('승객까지 이동 후 배터리', BATTERY)
     if BATTERY <= 0:
         break
     MATRIX[x][y] = 0
     CAR_POS = (x, y)
     # 2. 목적지로 데려다주기
     move_dis = ride(pid)
-    # print('목적지까지 거리', move_dis)
     BATTERY -= move_dis
     if BATTERY < 0:
         break
     BATTERY += move_dis*2
     FINISHED[pid] = 1
     CAR_POS = PASSENGER_DEST[pid]
-    # print('승객 데려다준 후 배터리', BATTERY)
-    # print('턴 종료 후 맵')
-    # for _ in MATRIX:
-    #     print(_)
 if BATTERY < 0:
     BATTERY = -1
 ALL_FINISHED = True
